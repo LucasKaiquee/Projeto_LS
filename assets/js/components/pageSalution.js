17a4cab6ea1ea6c227bda374f048fd5a74981c03
Luiz Fernando <fernando.albuquerque@academico.ifpb.edu.br>
2023-10-21 15:50:32 -0300

    feat: add pages reuso e impactos

--- assets/js/components/pageSalution.js
+++ assets/js/components/pageSalution.js
@@ -1,15 +1,15 @@
-const pageSolution = () => {
+const pageSolution = (solutionType) => {
     return `    
         <section class="solution-description">
-                <h1>Titulo da solução</h1>
+                <h1>${solutionType.title}</h1>
         
                 <div class="solution-content">
                     <div class="solution-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit vero, iste ipsam optio veritatis assumenda voluptatibus consequuntur ullam quam dolore consectetur reprehenderit in, debitis vitae molestias, illum omnis doloribus velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, impedit aliquam laborum voluptatum earum illo tempora, blanditiis minus ipsa dolore harum, reprehenderit ut assumenda sunt odit. Adipisci maxime eligendi eaque?</p>
+                        <p>${solutionType.para1}</p>
                     </div>
             
                     <div class="solution-info-2">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit vero, iste ipsam optio veritatis assumenda voluptatibus consequuntur ullam quam dolore consectetur reprehenderit in, debitis vitae molestias, illum omnis doloribus velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, impedit aliquam laborum voluptatum earum illo tempora, blanditiis minus ipsa dolore harum, reprehenderit ut assumenda sunt odit. Adipisci maxime eligendi eaque?</p>
+                        <p>${solutionType.para2}</p>
                     </div>
                 </div>
         
@@ -17,7 +17,7 @@ const pageSolution = () => {
         <div class="page-content">
             <h2>Como a Securitas Environment pode te ajudar ? </h2>
             <div class="page-info">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reiciendis, voluptas cupiditate necessitatibus cumque beatae quibusdam quam soluta fugit ex aliquid excepturi. Nostrum voluptates deleniti, dolorum iusto nemo molestias itaque.
+                <p>Ao facilitar a monitorização do consumo de recursos, a redução do desperdício e a adoção de práticas eco-amigáveis, nossa empresa inspira ações ecológicas, capacitando indivíduos a fazerem escolhas informadas e positivas em prol do nosso planeta.
                 </p>
     
                 <a href="" class="btn">Contate-nos</a>
@@ -25,7 +25,9 @@ const pageSolution = () => {
     
             <h5>Confira nossas dicas.</h5>
     
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/mdLL9_vjVb8?si=Wa4QcqmK65QEua7p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div class="iframe">
+            ${solutionType.iframe}
+            </div
         </div>
     `
 }
