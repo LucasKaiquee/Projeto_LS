74ff94721f26c753c1a0d7ce6ab6c9c1fccd0265
LucasKaiquee <lucaskaique743@gmail.com>
2023-10-21 12:28:26 -0300

    Style: add background para mobile

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,17 +2,17 @@ const serviceData = [
     {
         name: 'Recicle e Reuse o lixo no seu Campus:',
         description: 'O lixo é um dos maiores problemas ambientais do mundo. A produção de resíduos sólidos cresce a cada ano, e o descarte inadequado pode causar danos irreversíveis ao meio ambiente. O reuso e a conscientização sobre lixo são duas práticas essenciais para a redução do impacto ambiental causado pelo lixo. Confira algumas ações que você pode adotar!',
-        path: './pages/desenvolvimento.html'
+        path: './pages/page.html'
     },
     {
         name: 'Descarte corretamente o seu lixo:',
         description: 'O descarte correto do lixo é fundamental para a proteção do meio ambiente e da saúde pública. Ao descartar o lixo de forma correta, estamos evitando que ele contamine o solo, a água e o ar, causando diversos problemas ambientais e de saúde. Confira mais informações acerca do descarte incorreto do lixo:',
-        path: './pages/seguranca.html'
+        path: './pages/page.html'
     },
     {
         name: 'Securitas Environment na Mídia:',
         description: 'Nossa consultoria de TI oferece orientação especializada para ajudar sua empresa a tomar decisões estratégicas relacionadas à tecnologia da informação. Nossa equipe de consultores experientes analisa suas necessidades e objetivos, avalia sua infraestrutura atual e propõe soluções sob medida para otimizar seus processos de negócio, aumentar a eficiência operacional e impulsionar a inovação tecnológica.',
-        path: './pages/consultoria.html'
+        path: './pages/page.html'
     }
 ]
 
